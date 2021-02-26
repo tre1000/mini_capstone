8785cb941e262f4501279f3308aae15ef2643126
Tyler Epstein <tylerepstein27@gmail.com>
2021-02-26 13:52:21 -0500

    adds path to query by param and path to query by widlcard

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,18 @@
 class Api::ProductsController < ApplicationController
-  def available_products_action
+  def all_products_action
     @products = Product.all
     render "available_products.json.jb"
+  end
+
+  def product_by_id_query
+    id = params[:id]
+    @product = Product.find_by(id: id)
+    render "view_by_id.json.jb"
+  end
+
+  def product_by_id
+    id = params[:id]
+    @product = Product.find_by(id: id)
+    render "view_by_id.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/available_products" => "products#available_products_action"
+    get "/available_products" => "products#all_products_action"
+    get "/available_products_query" => "products#product_by_id_query"
+    get "/available_products/:id" => "products#product_by_id"
   end
 end
