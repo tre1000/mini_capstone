af0d71f4895336664235dc9b0bdbf2b63d953745
Tyler Epstein <tylerepstein27@gmail.com>
2021-03-03 13:19:16 -0500

    changes data types of price and description to decimal and text respectively. also adds stock column.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    price <= 20
+  end
+
+  def tax
+    tax = price * 0.09
+  end
+
+  def total
+    total = price + (price * 0.09)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_194829) do
+ActiveRecord::Schema.define(version: 2021_03_03_175211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 6, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "stock"
   end
 
 end
