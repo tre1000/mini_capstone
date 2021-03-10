cf107a7c89d309cf2355820c602817bfb63551c2
Tyler Epstein <tylerepstein27@gmail.com>
2021-03-10 13:41:10 -0500

    adds ability to index orders by current user

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,7 +3,7 @@ class Api::OrdersController < ApplicationController
     if current_user
       @order = Order.new(
         quantity: params[:quantity],
-        product_id: params[:id],
+        product_id: params[:product_id],
         user_id: current_user.id,
       )
       @order.save
@@ -11,5 +11,10 @@ class Api::OrdersController < ApplicationController
     else
       render json: { error: "You must be logged in to do that." }
     end
+  end
+
+  def index
+    @orders = current_user.orders
+    render "index.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,19 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    #CRUD paths
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "products/:id" => "products#delete"
-    post "products/:id/order" => "orders#create"
     #-----------------------------------------
+    #Authentication paths
     post "/users" => "users#create"
     post "sessions/" => "sessions#create"
     #-----------------------------------------
+    #Order paths
+    post "orders" => "orders#create"
+    get "orders" => "orders#index"
   end
 end
